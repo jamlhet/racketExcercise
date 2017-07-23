e19b2fbdc728616c11b71a1d549863be05e69941
Juan Carlos Rubiano Panadero <jamlhet@gmail.com>
2017-07-23 16:45:49 -0500

    todo por hoy

--- Exercise1_task3.rkt
+++ Exercise1_task3.rkt
@@ -1,1 +1,14 @@
 #lang racket
+(define (sq x) (* x x))
+(define (2bl x) (* 2 x))
+(define (new_fun fun1 fun2) (lambda (x) (fun2 (fun1 x))))
+;((new_fun sq mul2bl) 10)
+;(lambda (f) (lambda (x) (f x)))
+
+(define (n) (lambda (x) (x x)))
+(define (philo fun1 fun2) (lambda (x) (fun1 (fun2 x))))
+(n)
+(philo (n) (+ n 1))
+
+(define (eval-with env) 
+  (lambda (exp) (eval exp env)))
